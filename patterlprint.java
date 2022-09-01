da342513cfd2ef5fbb2d1d4da438bdb41563553b
Himanshu Shukla <111347254+Hemu123456789@users.noreply.github.com>
2022-09-01 20:23:29 +0530

    Create patterlprint.java

---
+++ patterlprint.java
@@ -0,0 +1,24 @@
+package pattern.java;
+
+public class patternL{
+public static void main(String[] args)
+{
+int n =5;
+
+  // Print i rows of stars
+
+    for (int i=1; i<=n; i++)
+    {
+    	 // Printing Column of Stars
+        for (int j=1; j<=i; j++)
+        {
+    
+            if( j == 1 || j == 0 || i == n)
+                System.out.print("*");
+            else
+                System.out.print("This is pattern");
+        }
+        System.out.println();
+    }
+}
+}
